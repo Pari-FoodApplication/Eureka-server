b2ec7b590e28c5c1447f9597d8acbe1930b00e8a
Pariram140 <80884996+Pariram140@users.noreply.github.com>
2023-12-30 13:15:55 +0530

    Initial Commit

---
+++ src/test/java/com/pari/foodapp/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pari.foodapp.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
